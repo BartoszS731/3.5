57ba799bcb1edcfcd96ea805501d212d7a2cb86a
BartoszS731 <161606110+BartoszS731@users.noreply.github.com>
2024-06-19 20:26:13 +0000

    3.4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,15 +5,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Podaj wysokość trójkąta:");
+        System.out.println("Podaj szerokość prostokąta:");
+        int width = scanner.nextInt();
+
+        System.out.println("Podaj wysokość prostokąta:");
         int height = scanner.nextInt();
 
-        for (int i = 1; i <= height; i++) {
-            for (int j = 0; j < height - i; j++) {
-                System.out.print(" ");
-            }
-            for (int j = 0; j < i; j++) {
-                System.out.print("*");
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (i == 0 || i == height - 1 || j == 0 || j == width - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
